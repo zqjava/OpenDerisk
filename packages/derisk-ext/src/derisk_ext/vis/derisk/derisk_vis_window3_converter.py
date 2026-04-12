13e483c5c7a6ab0e0733baf7a86dbd1cc4788f4e
ZQ <qq776905833@163.com>
2026-04-12 17:15:10 +0800

    修改本地配置

--- packages/derisk-ext/src/derisk_ext/vis/derisk/derisk_vis_window3_converter.py
+++ packages/derisk-ext/src/derisk_ext/vis/derisk/derisk_vis_window3_converter.py
@@ -189,10 +189,13 @@ class DeriskIncrVisWindow3Converter(DeriskVisIncrConverter):
     ):
         ## 并行情况下搜集当前运行中Agent信息
         running_agents: List[str] = []
-        for k, v in senders_map.items():
-            agent_state =  v.agent_state
-            if agent_state == Status.RUNNING:
-                running_agents.append(v.info.name)
+        agent_state = senders_map.get('agent_state')
+        if agent_state == Status.RUNNING:
+            running_agents.append(senders_map.get('simple_chat').info.name)
+        # for k, v in senders_map.items():
+        #     # agent_state =  v.agent_state
+        #     if agent_state == Status.RUNNING:
+        #         running_agents.append(v.info.name)
 
         task_manager: TreeManager = kwargs.get("task_manager")
         event_manager = kwargs.get("event_manager")
