c69a28d80784991306294dc31a918a958b75ef72
ZQ <qq776905833@163.com>
2026-05-07 21:07:06 +0800

    planning_agent.py report_agent.py team_auto_plan.py team_react_plan.py  打开注释

--- packages/derisk-serve/src/derisk_serve/building/app/service/service.py
+++ packages/derisk-serve/src/derisk_serve/building/app/service/service.py
@@ -666,7 +666,7 @@ class Service(BaseService[ServeEntity, ServeRequest, ServerResponse]):
                 f"[APP_DETAIL][PERF] _resource_to_app_detail 查询关联app[{app_code}]耗时: {(time.time() - _query_start) * 1000:.2f}ms"
             )
             logger.info(
-                f"[APP_DETAIL][PERF] _resource_to_app_detail 查询关联app_info[{app_info}]耗时: {(time.time() - _query_start) * 1000:.2f}ms"
+                f"[APP_DETAIL][PERF] _resource_to_app_detail 查询关联app[{app_code},app_info[{app_info}]耗时: {(time.time() - _query_start) * 1000:.2f}ms"
             )
             details.append(
                 GptsAppDetail(
